0b10e120aa4f2fdfe7717f85a05e1b857cec2140
Alexis Sellier <alexis@cloudhead.io>
2016-10-10 01:04:57 +0200

    Fix version test

--- Test.hs
+++ Test.hs
@@ -55,7 +55,7 @@ main = do
 versionMajor, versionMinor, versionRevision :: Int
 versionMajor    = 3
 versionMinor    = 1
-versionRevision = 0
+versionRevision = 1
 
 giveItTime :: IO ()
 giveItTime = threadDelay 500000
